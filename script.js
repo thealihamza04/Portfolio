eec2d31016fc27b5cfb994e20da401717b65351d
ali123606 <ah0681988@gmail.com>
2024-12-24 11:11:32 +0500

    developer -> dev

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ ScrollReveal().reveal('.home-content p, .about-content  ', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer', 'UI/UX Designer'],
+    strings: ['Frontend Dev', 'Backend Dev', 'UI/UX Designer'],
     typeSpeed: 100,
     backSpeed: 10,
     backDelay: 1000,
