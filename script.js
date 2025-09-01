6452fc0894907699beeb14a84e96b89d65a62f72
The Ali Hamza <ah0681988@gmail.com>
2025-09-01 22:13:28 +0500

    Add blog section with styling

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .blog-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content  ', { origin: 'right' });
 
